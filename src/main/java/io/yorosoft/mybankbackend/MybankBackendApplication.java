2e7fff09a6324d303450193883d7a1314921fd2a
Codeur47 <yoropapers@gmail.com>
2022-03-28 21:50:07 +0200

    first commit

---
+++ src/main/java/io/yorosoft/mybankbackend/MybankBackendApplication.java
@@ -0,0 +1,13 @@
+package io.yorosoft.mybankbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybankBackendApplication.class, args);
+	}
+
+}
